853c7b5d2ce4567a5b042f168c497728db20b436
Prashant <prashantsherkar7515400@gmail.com>
2025-04-21 00:22:53 +0530

    pushing remaining changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 
+
 export const app = express();
 
 dotenv.config({
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,30 @@
 import { app } from "./index.ts";
 import { connectDB } from "./db/database.ts";
+import WebSocket, {  WebSocketServer } from "ws";
+import { server } from "websocket";
+
 
 connectDB();
 
 const PORT = process.env.PORT || 8000;
-if (!PORT) {
-    console.log('Port undefined');
-}
-else {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running at port: ${PORT}`)
-    })
-}
+const httpServer = app.listen(PORT, () => console.log('http server running'));
 
-// console.log("hello world");
+const wss = new WebSocketServer({ server: httpServer });
+
+     
+
+wss.on('connection', (socket) => {
+    socket.on("error", err => console.log(err));
+
+    socket.on('message', function message(data, isBinary){
+        wss.clients.forEach(function each(client) {
+            if (client.readyState == WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+
+        }
+            
+        })
+        
+    })
+    
+})
